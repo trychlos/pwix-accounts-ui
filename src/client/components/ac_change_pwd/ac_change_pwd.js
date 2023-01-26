3e48359a0ca40aa77053ea020fb639688e646534
Pierre Wieser <pwieser@trychlos.org>
2023-01-26 15:13:12 +0100

    ac_change_pwd: fix typos

--- src/client/components/ac_change_pwd/ac_change_pwd.js
+++ src/client/components/ac_change_pwd/ac_change_pwd.js
@@ -16,24 +16,8 @@ Template.ac_change_pwd.onCreated( function(){
     const self = this;
 
     self.AC = {
-        error: new ReactiveVar( '' ),
         passwordOk: new ReactiveVar( true ),
-        twiceOk: new ReactiveVar( true ),
-
-        enableSubmit: function(){
-            const pwd2 = self.$( '.ac-newone .ac-input' ).val();
-            const pwd3 = self.$( '.ac-newtwo .ac-input' ).val();
-            self.AC.error.set( '' );
-            // whether the old password 'pwd1' is ok will be validated by the Meteor.Accounts package
-            // whether the new password 'pwd2' is ok is checked by the input password component
-            // we have to check that the two occurences 'pwd2' and 'pwd3' are the same
-            const equalsOk = pwd2 === pwd3;
-            //console.log( 'pwd2='+pwd2, 'pwd3='+pwd3, 'equalsOk='+equalsOk );
-            if( !equalsOk ){
-                self.AC.error.set( '<p>'+pwiI18n.label( pwiAccounts.strings, 'user', 'password_different' )+'</p>' );
-            }
-            btn.prop( 'disabled', !( pwd2.length && pwd3.length && equalsOk ));
-        },
+        twiceOk: new ReactiveVar( true )
     };
 });
 
@@ -43,13 +27,13 @@ Template.ac_change_pwd.onRendered( function(){
     self.autorun(() => {
         const btn = self.$( '.ac-change-pwd' ).closest( '.acUserLogin' ).find( '.ac-submit' );
         btn.prop( 'disabled', !self.AC.passwordOk.get() || !self.AC.twiceOk.get());
-    })
+    });
 });
 
 Template.ac_change_pwd.helpers({
     // error message
     errorMsg(){
-        return Template.instance().AC.error.get() || this.display.errorMsg();
+        return this.display.errorMsg();
     },
 
     // parameters for the password input
@@ -87,13 +71,13 @@ Template.ac_change_pwd.events({
     // message sent by the input password component
     //  NB: happens that data arrives undefined :( see #24
     'ac-password-data .ac-change-pwd'( event, instance, data ){
-        console.log( 'ac-password-data', data );
+        //console.log( 'ac-password-data', data );
         instance.AC.passwordOk.set( data ? data.ok : false );
     },
 
     // message sent by the twice passwords component
     'ac-twice-data .ac-change-pwd'( event, instance, data ){
-        console.log( 'ac-twice-data', data );
+        //console.log( 'ac-twice-data', data );
         instance.AC.twiceOk.set( data ? data.ok : false );
     }
 });
