1ed0fe6cf99504abd81a342a932567043b8772bd
Pierre Wieser <pwieser@trychlos.org>
2023-06-06 11:01:17 +0200

    Improve i18n calls

--- src/common/js/i18n.js
+++ src/common/js/i18n.js
@@ -2,10 +2,10 @@
  * pwix:accounts/src/common/js/i18n.js
  */
 
-import { pwixI18n as i18n } from 'meteor/pwix:i18n';
+import { pwixI18n } from 'meteor/pwix:i18n';
 
 import '../i18n/en_US.js';
-i18n.namespace( AC_I18N, 'en_US', pwiAccounts.i18n.en_US );
+pwixI18n.namespace( AC_I18N, 'en_US', pwiAccounts.i18n.en_US );
 
 import '../i18n/fr_FR.js';
-i18n.namespace( AC_I18N, 'fr_FR', pwiAccounts.i18n.fr_FR );
+pwixI18n.namespace( AC_I18N, 'fr_FR', pwiAccounts.i18n.fr_FR );
